70e093a16f66221765850b302cb830bfa5b27957
DanielCardenas <dfcardenas96@gmail.com>
2023-08-01 00:29:31 -0500

    v1

---
+++ app/src/main/java/com/example/appnew/AuthActivity.kt
@@ -0,0 +1,29 @@
+package com.example.appnew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+
+    //Analytics event
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Integracion de Firebase completa")
+        analytics.logEvent("InitScreen",bundle)
+
+    //Setup
+    setup()
+    }
+
+    private fun setup(){
+        title = "Autenticacion"
+
+
+    }
+
+}
