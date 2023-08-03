c0fa604a0d0898e872997144720369f8d1d28439
DanielCardenas <dfcardenas96@gmail.com>
2023-08-03 18:14:14 -0500

    :tada: Proyecto con Login Funcional

--- app/src/main/java/com/example/appnew/AuthActivity.kt
+++ app/src/main/java/com/example/appnew/AuthActivity.kt
@@ -1,30 +1,83 @@
 package com.example.appnew
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appnew.databinding.ActivityAuthBinding
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-    //Analytics event
+        //Analytics event
         val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val bundle = Bundle()
-        bundle.putString("message","Integracion de Firebase completa")
-        analytics.logEvent("InitScreen",bundle)
+        bundle.putString("message", "Integracion de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
 
-    //Setup
-    setup()
+        //Setup
+        setup()
     }
 
-    private fun setup(){
+    private fun setup() {
         title = "Autenticacion"
         ///singUpButton.setOnclick
-        singUp
+        binding.singUpButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(
+                        binding.emailEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+        binding.LoginButton.setOnClickListener {
+            if (binding.emailEditText.text.isNotEmpty() && binding.passwordEditText.text.isNotEmpty()) {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(
+                        binding.emailEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
     }
 
+    private fun showAlert() {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
 }
--- app/src/main/java/com/example/appnew/HomeActivity.kt
+++ app/src/main/java/com/example/appnew/HomeActivity.kt
@@ -1,11 +1,36 @@
 package com.example.appnew
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appnew.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+
+
+enum class ProviderType {
+    BASIC
+}
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+        title = "Inicio"
+        binding.emailTextView.text = email
+        binding.providerTextView.text = provider
+
+        binding.logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
