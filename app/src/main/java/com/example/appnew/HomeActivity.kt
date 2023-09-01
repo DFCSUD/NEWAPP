140b41661d78589f054ef0b963d76917309e39eb
DanielCardenas <dfcardenas96@gmail.com>
2023-09-01 17:17:42 -0500

    Update con BD

--- app/src/main/java/com/example/appnew/HomeActivity.kt
+++ app/src/main/java/com/example/appnew/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appnew.databinding.ActivityHomeBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 enum class ProviderType {
@@ -11,6 +13,9 @@ enum class ProviderType {
 }
 
 class HomeActivity : AppCompatActivity() {
+
+    private val db = FirebaseFirestore.getInstance()
+
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,7 @@ class HomeActivity : AppCompatActivity() {
         val bundle = intent.extras
         val email: String? = bundle?.getString("email")
         val provider: String? = bundle?.getString("provider")
+
         setup(email ?: "", provider ?: "")
     }
 
@@ -32,5 +38,26 @@ class HomeActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signOut()
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.saveBtn.setOnClickListener{
+            db.collection("users").document(email).set(
+                hashMapOf("provider" to provider,
+                            "address" to binding.address.text.toString(),
+                            "phone" to binding.phoneTxtView.text.toString(),
+                            "nameUser" to binding.nameUser.text.toString())
+            )
+
+        }
+        binding.consultarBtn.setOnClickListener{
+            db.collection("users").document(email).get().addOnSuccessListener {
+                binding.nameUser.setText(it.get("nameUser") as String?)
+                binding.address.setText(it.get("address") as String?)
+                binding.phoneTxtView.setText(it.get("phone") as String?)
+            }
+
+        }
+
+
     }
+
 }
